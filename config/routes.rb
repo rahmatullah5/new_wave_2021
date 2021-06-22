c63e7313a32cf20f8ca78008d88f7b4960959b35
Muhammad Rahmatullah <muhammad.rahmatullah@mekari.com>
2021-06-23 00:59:59 +0700

    Modify route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :comments
-  resources :posts
-  resources :users
+  resources :users do
+    resources :posts do
+      resources :comments
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
