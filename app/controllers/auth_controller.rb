39d883de53bc8cb5d0f217a4f43a32e9932ba31d
Muhammad Rahmatullah <muhammad.rahmatullah@mekari.com>
2021-06-23 01:49:15 +0700

    Optimize code

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class AuthController < ApplicationController
   skip_before_action :authorize_request
   def create
     user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
+    if user&.authenticate(params[:password])
       render json: { token: Jwt::Authenticator.encode(user_id: user.id) }, status: :ok
     else
       render json: {}, status: :unauthorized
