ab82dcee201dc0cf5e2067431d649f69656bf2a4
Muhammad Rahmatullah <muhammad.rahmatullah@mekari.com>
2021-06-23 01:16:20 +0700

    Simplified route

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@
 
 Rails.application.routes.draw do
   resources :users do
-    resources :posts do
-      resources :comments
-    end
+  resources :posts do
+    resources :comments
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
